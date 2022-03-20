6b6b17cd0db7049a9c279833bb7bb5cda603e144
Camden Adams <adamscamden2@gmail.com>
2022-03-20 11:19:23 -1000

    Input system redo

--- input.py
+++ input.py
@@ -1,11 +1,113 @@
-class Movement:
-    def __init__(self):
-        self.func = []
+import math
 
-    def add(self, code, func):
-        self.func.append({'code': code, 'func': func})
-    
-    def run(self, keys):
-        for func in self.func:
-            if keys[func['code']]:
-                func['func']()
+class Movement:
+	def __init__(self, entity_manager, collisions=[], speed = 10, gravity = 3):
+		self.func = []
+		self.entity_manager = entity_manager
+		self.collision = collisions
+		self.player_id = 0
+		self.player = entity_manager.entities[self.player_id]
+
+		try:
+			self.player_ground_collision_layer = self.collision[self.player.z_floor()-1]
+		except IndexError:
+			self.player_ground_collision_layer = []
+		try:
+			self.player_collision_layer = self.collision[self.player.z_floor()]
+		except IndexError:
+			self.player_collision_layer = []
+		
+		self.player_speed = speed
+		self.gravity_speed = gravity
+
+	def add(self, code, func):
+		self.func.append({'code': code, 'func': func})
+
+	def update(self, entity_manager):
+		self.entity_manager = entity_manager
+		self.player = entity_manager.entities[self.player_id]
+		try:
+			self.player_ground_collision_layer = self.collision[self.player.z_floor()-1]
+		except IndexError:
+			self.player_ground_collision_layer = []
+		try:
+			self.player_collision_layer = self.collision[self.player.z_floor()]
+		except IndexError:
+			self.player_collision_layer = []
+	
+	def run(self, keys, entity_manager, delta_time):
+		self.update(entity_manager)
+		for func in self.func:
+			if keys[func['code']]:
+				# if self.player.falling:
+				#     continue
+				func['func'](self, delta_time)
+		
+		self.gravity(delta_time) 
+
+	def gravity(self, delta_time):
+		if (self.player.x_floor(), self.player.y_floor()) not in self.player_ground_collision_layer:
+			self.player.falling = True
+			self.player.z -= self.gravity_speed * delta_time
+		else:
+			self.player.falling = False
+
+	def UP(self, delta_time):
+		local_speed = self.player_speed * delta_time
+		target = (self.player.x_floor(), math.floor(self.player.y - local_speed))
+		if target not in self.player_collision_layer:
+			self.player.y -= local_speed
+		else:
+			target_type = self.player_collision_layer[self.player_collision_layer.index(target) + 1]
+			if target_type == 'solid':
+				pass
+			elif target_type == 'stairs_north':
+				# go up stairs
+				pass
+			elif 'stairs' in target_type:
+				pass
+
+	def DOWN(self, delta_time):
+		local_speed = self.player_speed * delta_time
+		target = (self.player.x_floor(), math.floor(self.player.y + local_speed))
+		if target not in self.player_collision_layer:
+			self.player.y += local_speed
+		else:
+			target_type = self.player_collision_layer[self.player_collision_layer.index(target) + 1]
+			if target_type == 'solid':
+				pass
+			elif target_type == 'stairs_south':
+				# go up stairs
+				pass
+			elif 'stairs' in target_type:
+				pass
+
+	def LEFT(self, delta_time):
+		local_speed = self.player_speed * delta_time
+		target = (math.floor(self.player.x - local_speed), self.player.y_floor())
+		if target not in self.player_collision_layer:
+			self.player.x -= local_speed
+		else:
+			target_type = self.player_collision_layer[self.player_collision_layer.index(target) + 1]
+			if target_type == 'solid':
+				pass
+			elif target_type == 'stairs_east':
+				# go up stairs
+				pass
+			elif 'stairs' in target_type:
+				pass
+
+	def RIGHT(self, delta_time):
+		local_speed = self.player_speed * delta_time
+		target = (math.floor(self.player.x + local_speed), self.player.y_floor())
+		if target not in self.player_collision_layer:
+			self.player.x += local_speed
+		else:
+			target_type = self.player_collision_layer[self.player_collision_layer.index(target) + 1]
+			if target_type == 'solid':
+				pass
+			elif target_type == 'stairs_west':
+				# go up stairs
+				pass
+			elif 'stairs' in target_type:
+				pass
