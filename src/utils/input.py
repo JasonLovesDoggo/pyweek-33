1ac5a6ac1590951d57526b101dbe05786ef54e74
Camden Adams <adamscamden2@gmail.com>
2022-03-20 18:01:39 -1000

    New config options and setup refactor

--- src/utils/input.py
+++ src/utils/input.py
@@ -4,136 +4,138 @@ from src.utils.tools import is_negative
 
 
 class Movement:
-    def __init__(self, entity_manager, collisions=[], speed=10, gravity=3):
-        self.func = []
-        self.entity_manager = entity_manager
-        self.collision = collisions
-        self.player_id = 0
-        self.player = entity_manager.entities[self.player_id]
+	def __init__(self, entity_manager, collisions=[], speed=10, gravity=3):
+		self.func = []
+		self.entity_manager = entity_manager
+		self.collision = collisions
+		self.player_id = 0
+		self.player = entity_manager.entities[self.player_id]
 
-        try:
-            self.player_ground_collision_layer = self.collision[
-                self.player.z_floor() - 1
-            ]
-        except IndexError:
-            self.player_ground_collision_layer = []
-        try:
-            self.player_collision_layer = self.collision[self.player.z_floor()]
-        except IndexError:
-            self.player_collision_layer = []
+		try:
+			self.player_ground_collision_layer = self.collision[
+				self.player.z_floor() - 1
+			]
+		except IndexError:
+			self.player_ground_collision_layer = []
+		try:
+			self.player_collision_layer = self.collision[self.player.z_floor()]
+		except IndexError:
+			self.player_collision_layer = []
 
-        self.player_speed = speed
-        self.gravity_speed = gravity
+		self.player_speed = speed
+		self.gravity_speed = gravity
 
-    def add(self, code, func):
-        self.func.append({"code": code, "func": func})
+	def add(self, code, func):
+		self.func.append({"code": code, "func": func})
 
-    def update(self, entity_manager):
-        self.entity_manager = entity_manager
-        self.player = entity_manager.entities[self.player_id]
-        try:
-            self.player_ground_collision_layer = self.collision[
-                self.player.z_floor() - 1
-            ]
-        except IndexError:
-            self.player_ground_collision_layer = []
-        try:
-            self.player_collision_layer = self.collision[self.player.z_floor()]
-        except IndexError:
-            self.player_collision_layer = []
+	def update(self, entity_manager):
+		self.entity_manager = entity_manager
+		self.player = entity_manager.entities[self.player_id]
+		try:
+			self.player_ground_collision_layer = self.collision[
+				self.player.z_floor() - 1
+			]
+		except IndexError:
+			self.player_ground_collision_layer = []
+		try:
+			self.player_collision_layer = self.collision[self.player.z_floor()]
+		except IndexError:
+			self.player_collision_layer = []
 
-    def run(self, keys, entity_manager, delta_time):
-        self.update(entity_manager)
+	def run(self, keys, entity_manager, delta_time, offset):
+		self.update(entity_manager)
 
-        used = []
-        for func in self.func:
-            if keys[func["code"]] and func["func"] not in used:
-                used.append(func["func"])
-                if self.player.falling:
-                    continue
-                func["func"](self, delta_time)
+		used = []
+		for func in self.func:
+			if keys[func["code"]] and func["func"] not in used:
+				used.append(func["func"])
+				if self.player.falling:
+					continue
+				func["func"](self, delta_time)
 
-        self.gravity(delta_time)
+		self.gravity(delta_time)
 
-    def gravity(self, delta_time):
-        if (
-            self.player.x_floor(),
-            self.player.y_floor(),
-        ) not in self.player_ground_collision_layer:
-            self.player.falling = True
-            self.player.z -= self.gravity_speed * delta_time
-            if is_negative(self.player.z):
-                self.player.deadcount += 1
-                if self.player.deadcount > 40:
-                    pass  # implement death/title screen
-                pass
-        else:
-            self.player.falling = False
+		return offset
 
-    def UP(self, delta_time):
-        local_speed = self.player_speed * delta_time
-        target = (self.player.x_floor(), math.floor(self.player.y - local_speed))
-        if target not in self.player_collision_layer:
-            self.player.y -= local_speed
-        else:
-            target_type = self.player_collision_layer[
-                self.player_collision_layer.index(target) + 1
-            ]
-            if target_type == "solid":
-                pass
-            elif target_type == "stairs_north":
-                # go up stairs
-                pass
-            elif "stairs" in target_type:
-                pass
+	def gravity(self, delta_time):
+		if (
+			self.player.x_floor(),
+			self.player.y_floor(),
+		) not in self.player_ground_collision_layer:
+			self.player.falling = True
+			self.player.z -= self.gravity_speed * delta_time
+			if is_negative(self.player.z):
+				self.player.deadcount += 1
+				if self.player.deadcount > 40:
+					pass  # implement death/title screen
+				pass
+		else:
+			self.player.falling = False
 
-    def DOWN(self, delta_time):
-        local_speed = self.player_speed * delta_time
-        target = (self.player.x_floor(), math.floor(self.player.y + local_speed))
-        if target not in self.player_collision_layer:
-            self.player.y += local_speed
-        else:
-            target_type = self.player_collision_layer[
-                self.player_collision_layer.index(target) + 1
-            ]
-            if target_type == "solid":
-                pass
-            elif target_type == "stairs_south":
-                # go up stairs
-                pass
-            elif "stairs" in target_type:
-                pass
+	def UP(self, delta_time):
+		local_speed = self.player_speed * delta_time
+		target = (self.player.x_floor(), math.floor(self.player.y - local_speed))
+		if target not in self.player_collision_layer:
+			self.player.y -= local_speed
+		else:
+			target_type = self.player_collision_layer[
+				self.player_collision_layer.index(target) + 1
+			]
+			if target_type == "solid":
+				pass
+			elif target_type == "stairs_north":
+				# go up stairs
+				pass
+			elif "stairs" in target_type:
+				pass
 
-    def LEFT(self, delta_time):
-        local_speed = self.player_speed * delta_time
-        target = (math.floor(self.player.x - local_speed), self.player.y_floor())
-        if target not in self.player_collision_layer:
-            self.player.x -= local_speed
-        else:
-            target_type = self.player_collision_layer[
-                self.player_collision_layer.index(target) + 1
-            ]
-            if target_type == "solid":
-                pass
-            elif target_type == "stairs_east":
-                # go up stairs
-                pass
-            elif "stairs" in target_type:
-                pass
+	def DOWN(self, delta_time):
+		local_speed = self.player_speed * delta_time
+		target = (self.player.x_floor(), math.floor(self.player.y + local_speed))
+		if target not in self.player_collision_layer:
+			self.player.y += local_speed
+		else:
+			target_type = self.player_collision_layer[
+				self.player_collision_layer.index(target) + 1
+			]
+			if target_type == "solid":
+				pass
+			elif target_type == "stairs_south":
+				# go up stairs
+				pass
+			elif "stairs" in target_type:
+				pass
 
-    def RIGHT(self, delta_time):
-        local_speed = self.player_speed * delta_time
-        target = (math.floor(self.player.x + local_speed), self.player.y_floor())
-        if target not in self.player_collision_layer:
-            self.player.x += local_speed
-        else:
-            target_type = self.player_collision_layer[
-                self.player_collision_layer.index(target) + 1
-            ]
-            if target_type == "solid":
-                pass
-            elif target_type == "stairs_west":
-                # go up stairs
-                pass
-            elif "stairs" in target_type:
-                pass
+	def LEFT(self, delta_time):
+		local_speed = self.player_speed * delta_time
+		target = (math.floor(self.player.x - local_speed), self.player.y_floor())
+		if target not in self.player_collision_layer:
+			self.player.x -= local_speed
+		else:
+			target_type = self.player_collision_layer[
+				self.player_collision_layer.index(target) + 1
+			]
+			if target_type == "solid":
+				pass
+			elif target_type == "stairs_east":
+				# go up stairs
+				pass
+			elif "stairs" in target_type:
+				pass
+
+	def RIGHT(self, delta_time):
+		local_speed = self.player_speed * delta_time
+		target = (math.floor(self.player.x + local_speed), self.player.y_floor())
+		if target not in self.player_collision_layer:
+			self.player.x += local_speed
+		else:
+			target_type = self.player_collision_layer[
+				self.player_collision_layer.index(target) + 1
+			]
+			if target_type == "solid":
+				pass
+			elif target_type == "stairs_west":
+				# go up stairs
+				pass
+			elif "stairs" in target_type:
+				pass
