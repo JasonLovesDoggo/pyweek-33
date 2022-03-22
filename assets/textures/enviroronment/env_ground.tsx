e74794e9976014cea7b6c3d5507d364ff340de4d
Camden Adams <adamscamden2@gmail.com>
2022-03-22 13:11:54 -1000

    Texture fix

---
+++ assets/textures/enviroronment/env_ground.tsx
@@ -0,0 +1,15 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.2" name="env_ground" tilewidth="20" tileheight="24" tilecount="2" columns="2">
+ <tileoffset x="0" y="14"/>
+ <image source="ground.png" width="40" height="24"/>
+ <tile id="0">
+  <objectgroup draworder="index" id="2">
+   <object id="1" type="solid" x="4.71818" y="5.0168" width="13.0198" height="17.4991"/>
+  </objectgroup>
+ </tile>
+ <tile id="1">
+  <objectgroup draworder="index" id="2">
+   <object id="1" type="solid" x="3.94177" y="3.34453" width="11.8253" height="17.6782"/>
+  </objectgroup>
+ </tile>
+</tileset>
---
+++ assets/textures/enviroronment/env_stairs.tsx
@@ -0,0 +1,5 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.2" name="env_stairs" tilewidth="20" tileheight="24" tilecount="4" columns="4">
+ <tileoffset x="0" y="14"/>
+ <image source="stairs.png" width="80" height="24"/>
+</tileset>
