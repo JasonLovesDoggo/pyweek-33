01a6131ac7c2f65aed740a587e8908dec85200ce
Jason <thebosssg19@gmail.com>
2022-03-22 21:32:42 -0400

    Updated audio engine More to do i just want to make sure this is public

--- src/utils/audio.py
+++ src/utils/audio.py
@@ -1,3 +1,5 @@
+import logging
+from os.path import exists
 from logging import getLogger
 from pygame import mixer
 
@@ -8,4 +10,19 @@ class AudioManager:
     def __init__(self) -> None:
         self.mixer = mixer
         self.mixer.init()
+        self.SongLoaded = False
         log.info("Initialized the audio mixer.")
+
+    def play(self):
+        if not self.SongLoaded:
+            log.warning(f'No song currently loaded!')
+
+        self.mixer.music.play()
+
+    def load(self, MusicFile: str):
+        if not exists(MusicFile):
+            log.error(f'Music file {MusicFile} does not exist!')
+
+        self.SongLoaded = True
+        self.mixer.music.load(MusicFile)
+
