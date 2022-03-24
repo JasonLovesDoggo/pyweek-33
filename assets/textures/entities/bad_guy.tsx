9bf2461de7905e04be1d7e8584d0f9a1e3c622b3
Camden Adams <adamscamden2@gmail.com>
2022-03-23 16:06:24 -1000

    Basic enemy class.

---
+++ assets/textures/entities/bad_guy.tsx
@@ -0,0 +1,5 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.2" name="bad_guy" tilewidth="20" tileheight="24" tilecount="1" columns="1">
+ <image source="bad_guy.png" width="20" height="24"/>
+ <tile id="0" type="enemy"/>
+</tileset>
